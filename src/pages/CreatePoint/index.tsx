cc4b18f29580d861f2bb99284d835dc4db9514da
Igor Sily <igorsily2@gmail.com>
2020-07-20 20:57:18 -0300

    Add upload image

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -8,6 +8,7 @@ import logo from "../../assets/logo.svg";
 import api from "../../services/api";
 import axios from "axios";
 import { LeafletMouseEvent } from "leaflet";
+import Dropzone from "../../components/Dropzone";
 
 interface Item {
   id: number;
@@ -45,6 +46,7 @@ const CreatePoint: React.FC = () => {
   ]);
 
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
+  const [selectedFile, setSelectedFile] = useState<File>();
 
   useEffect(() => {
     api.get("items").then((resp) => {
@@ -124,16 +126,20 @@ const CreatePoint: React.FC = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items,
-    };
+    const data = new FormData();
+
+    data.append("name", name);
+    data.append("email", email);
+    data.append("whatsapp", whatsapp);
+    data.append("uf", uf);
+    data.append("city", city);
+    data.append("latitude", String(latitude));
+    data.append("longitude", String(longitude));
+    data.append("items", items.join(","));
+
+    if (selectedFile) {
+      data.append("image", selectedFile);
+    }
 
     await api.post("points", data);
 
@@ -147,7 +153,6 @@ const CreatePoint: React.FC = () => {
       <header>
         <img alt="Ecoleta" src={logo} />
         <Link to="/">
-          {" "}
           <FiArrowLeft /> Voltar para a home
         </Link>
       </header>
@@ -157,6 +162,7 @@ const CreatePoint: React.FC = () => {
           ponto de coleta
         </h1>
 
+        <Dropzone onFileUploaded={setSelectedFile} />
         <fieldset>
           <legend>
             <h2>Dados</h2>
